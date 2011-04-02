ec918eda7d9b3c735887313b4596a55a5be14fdf
RL <lekkerduidelijk@gmail.com>
2011-04-02 11:41:33 +0200

    Initial commit of clean Rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookit::Application.config.secret_token = '10d05982a02365c5b3aa2b3137d177b6f2cea021d4b6428169c942a23ab8860e5fc08af6956866000dab9f9446cb83203ea5d3912f9f8d74b0850789ed8b2d43'
