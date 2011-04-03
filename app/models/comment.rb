95fbb29faca173a20a93bbadad66c3889f0f42b4
RL <lekkerduidelijk@gmail.com>
2011-04-03 18:31:30 +0200

    Changed comment association Added definition to check for ownership Added fieldset to form for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,12 @@
 class Comment < ActiveRecord::Base
-
   validates :body, :presence => true
   validates :tenant_id, :presence => true
-#  validates :tenant_id, :presence => true
-  
-  
-  belongs_to :user  
-  has_one :tenant
 
+  belongs_to :user  
+  belongs_to :tenant
+
+  def owned_by?(owner)
+    return false unless owner.is_a? User
+    user == owner
+  end
 end
