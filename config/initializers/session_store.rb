e178d272e6a7e7054ec3193ae4706ec2f080f62a
RL <lekkerduidelijk@gmail.com>
2011-04-03 18:21:21 +0200

    Adding sessions and authentication to application

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ Bookit::Application.config.session_store :cookie_store, :key => '_bookit_session
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Bookit::Application.config.session_store :active_record_store
+Bookit::Application.config.session_store :active_record_store
